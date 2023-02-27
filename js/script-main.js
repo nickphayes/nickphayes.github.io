36ce1cc0740b736a266673abba30f061d192cf35
nickphayes <nickphayes.14@gmail.com>
2023-02-27 01:01:52 +0000

    fixing homescreen

--- js/script-main.js
+++ js/script-main.js
@@ -18,6 +18,30 @@ function scrollWindow(selection){
       });
 }
 
+// typewriter function
+var i = 0 ; 
+function typeWriter(text, moveToTopLeft) {
+    if (i < text.length) {
+      document.getElementById("homename").textContent += text.charAt(i);
+      i++;
+      setTimeout(function() {
+        typeWriter(text, moveToTopLeft)
+      }, 100);
+    } else if (typeof moveToTopLeft == "function") {
+      setTimeout(moveToTopLeft, 0);
+    }
+  }
+  
+// moving title container from center to top left
+
+function moveToTopLeft() {
+    var div = document.querySelector(".title a");
+    div.style.position = "absolute";
+    div.style.left = "0";
+    div.style.top = "0";
+  }
+  
+//STOPPING POINT: FIXING MOVING HOMENAME
 
 
 
