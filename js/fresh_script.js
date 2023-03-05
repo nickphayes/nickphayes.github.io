a5d02fbaa3a45eb667dd6af7dee48eccf920669c
nickphayes <nickphayes.14@gmail.com>
2023-03-05 23:46:40 +0000

    redesign

--- js/fresh_script.js
+++ js/fresh_script.js
@@ -62,3 +62,23 @@ function fadeBackground() {
         fader.style.zIndex = '-1';
     }, 2000)
 }
+
+// arrow fade in and out on scroll
+
+$(window).scroll(function() {
+    if ($(this).scrollTop()> 5) {
+        $('.arrow').fadeOut();
+     }
+    else {
+      $('.arrow').fadeIn();
+     }
+ });
+
+ // setting smooth scrolling on click event
+// takes in target item as selection
+function scrollWindow(selection){
+    document.querySelector(selection).scrollIntoView({ 
+        behavior: 'smooth',
+        block: 'center'
+      });
+}
